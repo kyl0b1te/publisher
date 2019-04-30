e48bf3dd9614e62a1fd9962e575240e019ae93cb
zhikiri <zhikiri.dev@gmail.com>
2019-04-30 11:24:04 +0300

    finished the publisher flow

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,49 @@
-import { Sources } from './sources';
+import process from 'process';
+
+import { Publisher } from './publisher';
+
+const env = (key: string): string => {
+
+  const value = process.env[key];
+  if (!value) {
+
+    throw new Error(`Env variable isn't set: ${key}`);
+  }
+
+  return value;
+};
+
+const stopOnFail = (status: boolean, failMessage: string): void => {
+
+  if (!status) {
+    throw new Error(`[ERROR]: ${failMessage}`);
+  }
+}
 
 export async function publish(): Promise<void> {
 
-  const sources = new Sources({
-    // todo : get from ENV
-    Bucket: 'src-zhikiri.info',
-    Key: 'zhikiri.info.zip'
+  const publisher = new Publisher({
+    Bucket: env('SOURCE_BUCKET'),
+    Key: env('SOURCE_FILENAME'),
   });
 
   const tmpSourcePath = '/tmp/src';
 
-  const loaded = await sources.loadTo(tmpSourcePath);
-  console.log(`Finished with status: ${loaded}`);
+  const loaded = await publisher.loadTo(tmpSourcePath);
+  stopOnFail(loaded, 'Source file loading failed');
+  console.log('Source files was loaded');
 
-  if (loaded) {
-    sources.makeStaticFrom(tmpSourcePath);
-  }
+  const generated = await publisher.makeStaticFrom(
+    tmpSourcePath,
+    env('WEBSITE_URL'),
+    env('WEBSITE_THEME')
+  );
+  stopOnFail(generated, 'Static content generation failed');
+  console.log('Static content was generated');
+
+  const deployed = await publisher.deployFrom(tmpSourcePath);
+  stopOnFail(deployed, 'Static content deployment failed');
+  console.log('Changes was deployed');
+
+  return Promise.resolve();
 }
---
+++ src/publisher.ts
@@ -0,0 +1,83 @@
+import fs from 'fs';
+import path from 'path';
+import { spawn, exec, ExecException } from 'child_process';
+
+import { AWSError } from 'aws-sdk';
+import S3 from 'aws-sdk/clients/s3';
+
+export class Publisher {
+
+  private srcArchivePath = '/tmp/src.zip';
+
+  constructor(
+    private s3KeyParams: S3.GetObjectRequest,
+    private s3Config: S3.ClientConfiguration = { apiVersion: '2006-03-01' }
+  ) { }
+
+  async loadTo(srcPath: string): Promise<boolean> {
+
+    try {
+      const data = await this.load(this.s3KeyParams, this.s3Config);
+      await this.save(this.srcArchivePath, data);
+
+      return await this.unpack(this.srcArchivePath, srcPath);
+    } catch (err) {
+
+      return Promise.resolve(false);
+    }
+  }
+
+  makeStaticFrom(srcPath: string, website: string, theme: string): Promise<boolean> {
+
+    return this.execute(`./bin/hugo -s ${srcPath} -b ${website} -t ${theme}`);
+  }
+
+  deployFrom(srcPath: string): Promise<boolean> {
+
+    return this.execute('./bin/s3sync -c env')
+      .then(() => this.execute(`rm -rf ${srcPath}`));
+  }
+
+  private load(params: S3.GetObjectRequest, config: S3.ClientConfiguration): Promise<S3.Body> {
+
+    return new Promise((resolve, reject) => {
+
+      (new S3(config)).getObject(params, (err: AWSError, data: S3.GetObjectOutput) => {
+
+        return err != null ? reject(err) : resolve(data.Body);
+      });
+    });
+  }
+
+  private save(tmpFilePath: string, data: S3.Body): Promise<string> {
+
+    return new Promise((resolve, reject) => {
+
+      fs.writeFile(tmpFilePath, data, (err) => {
+
+        return err ? reject(err) : resolve(tmpFilePath);
+      });
+    });
+  }
+
+  private unpack(tmpFilePath: string, srcPath: string): Promise<boolean> {
+
+    return this.execute(`unzip -o -q ${tmpFilePath} -d ${srcPath}`)
+      .then(() => this.execute(`rm ${tmpFilePath}`));
+  }
+
+  private execute(command: string): Promise<boolean> {
+
+    return new Promise((resolve, reject) => {
+
+      exec(command, (error: ExecException | null, stdout: string) => {
+
+        if (stdout !== '') {
+          console.log(stdout);
+        }
+
+        return error != null ? reject(error) : resolve(true);
+      });
+    });
+  }
+}
