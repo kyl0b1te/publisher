80a9deb49975bbe0e7a6b66504c77dbdcf708967
zhikiri <zhikiri.dev@gmail.com>
2019-05-04 15:50:40 +0300

    added dotenv for lambda runtime

--- lambda.js
+++ lambda.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const dotenv = require('dotenv');
 const { publish } = require('./dist/index');
 
+dotenv.config('./.env');
+
 exports.publisher = async () => await publish();
-// (async () => await publish())();
