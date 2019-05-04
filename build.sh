ed30bdf1b5a80dce7f80bc1bc73be868b50762ab
zhikiri <zhikiri.dev@gmail.com>
2019-05-04 14:33:05 +0300

    added lambda the build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create build folder
+mkdir /tmp/build && cd /app
+
+# Copy sources except node_modules folder
+cp -r `ls -A | grep -v "node_modules"` /tmp/build/ && cd /tmp/build
+
+# Install production deps
+npm ci --only=production
+
+# Compile typescript and zip files
+zip -r /tmp/lambda.blog-publisher.zip lambda.js dist/ bin/ node_modules/ -x dist/cmd
+
+# Clean up
+rm -rf /tmp/build
